428cb0c86e4e07cd6207e1dbbdc7a93250bc943c
Ahegan <enzosimioni.eu@gmail.com>
2021-01-04 19:19:42 +0100

    input verification

--- BirthdayCalculator/ressources/js/app.js
+++ BirthdayCalculator/ressources/js/app.js
@@ -25,48 +25,57 @@ var yEnd;
 document.getElementById("dataUser").addEventListener("submit", function (e) {
     e.preventDefault();
     result.innerHTML = "";
-    if (true/* empty == 0 */) {
+
+    if (empty == 0) {
         month = input.namedItem("month").value;
         day = input.namedItem("day").value;
-        yStart = Number(input.namedItem("yStart").value);
-        yEnd = Number(input.namedItem("yEnd").value);
+        yStart = input.namedItem("yStart").value;
+        yEnd = input.namedItem("yEnd").value;
+        let x = new Date(`${yStart}-${month}-${day}T12:00:00`);
 
-        let x;
-        let y;
-        let result = document.getElementById("result");
+        if (Number.isInteger(Number(month)) && Number.isInteger(Number(day)) && Number.isInteger(Number(yStart)) && Number.isInteger(Number(yEnd)) && x != "Invalid Date")  {
+            let y;
+            let result = document.getElementById("result");
 
-        for (i = yStart; i <= yEnd; i++) {
+            for (i = yStart; i <= yEnd; i++) {
 
-            x = new Date(`${i}-${month}-${day}T12:00:00`);
-            x = x.toString();
-            console.log(`${i}-${month}-${day}T12:00:00`);
+                x = new Date(`${i}-${month}-${day}T12:00:00`);
+                x = x.toString();
 
-            if (x.substring(0, 3) == "Mon") {
-                y = "Monday"
-            }
-            if (x.substring(0, 3) == "Tue") {
-                y = "Tuesday"
-            }
-            if (x.substring(0, 3) == "Wen") {
-                y = "Wednsday"
-            }
-            if (x.substring(0, 3) == "Thu") {
-                y = "Thursday"
-            }
-            if (x.substring(0, 3) == "Fri") {
-                y = "Friday"
-            }
-            if (x.substring(0,3) == "Sat") {
-                y = "Saturday"
-            }
-            if (x.substring(0,3) == "Sun") {
-                y = "Sunday"
-            }
-            result.innerHTML += `In ${i}, your birthday will be on a ${y} <br>`;
-            
+                if (x.substring(0, 3) == "Mon") {
+                    y = "Monday"
+                }
+                if (x.substring(0, 3) == "Tue") {
+                    y = "Tuesday"
+                }
+                if (x.substring(0, 3) == "Wen") {
+                    y = "Wednsday"
+                }
+                if (x.substring(0, 3) == "Thu") {
+                    y = "Thursday"
+                }
+                if (x.substring(0, 3) == "Fri") {
+                    y = "Friday"
+                }
+                if (x.substring(0, 3) == "Sat") {
+                    y = "Saturday"
+                }
+                if (x.substring(0, 3) == "Sun") {
+                    y = "Sunday"
+                }
+                result.innerHTML += `In ${i}, your birthday will be on a ${y} <br>`;
 
+
+            }
         }
+        else {
+            document.getElementById("pop-up").style.display = "initial"
+        }
+
 
     }
 })
 
+function closepop() {
+    document.getElementById("pop-up").style.display = "none"
+}
