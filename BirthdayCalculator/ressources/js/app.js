d75597f3259012fa8576df5ec28a65a64c8dbd47
Ahegan <enzosimioni.eu@gmail.com>
2021-01-13 13:12:59 +0100

    optimisation update

--- BirthdayCalculator/ressources/js/app.js
+++ BirthdayCalculator/ressources/js/app.js
@@ -1,5 +1,6 @@
 var input = document.getElementById("dataUser").getElementsByTagName("input");
-var empty;
+var empty, year;
+
 
 //define error messages for empties inputs
 document.getElementById("dataUser").addEventListener("input", function () {
@@ -10,12 +11,18 @@ document.getElementById("dataUser").addEventListener("input", function () {
             document.getElementById(`span${i}`).style.opacity = "1";
 
         } else document.getElementById(`span${i}`).style.opacity = "0";
-        
+
 
     }
 
 })
-var month, day, yStart, yEnd;
+var month, day, yStart, yEnd, year;
+function displayModal(displayedText) {
+    document.getElementById("pop-up").style.visibility = "initial";
+    document.getElementById("pop-up").style.opacity = "1";
+    document.getElementById("window").style.top = "45%";
+    document.getElementById("alert-message").innerHTML = displayedText
+}
 
 //calculate and display the dates
 document.getElementById("dataUser").addEventListener("submit", function (e) {
@@ -23,23 +30,31 @@ document.getElementById("dataUser").addEventListener("submit", function (e) {
     result.innerHTML = "";
     var verifStart = "";
     var verifEnd = "";
+    var verifYear = "";
 
     if (empty === 0) {
         month = input.namedItem("month").value;
         day = input.namedItem("day").value;
         yStart = input.namedItem("yStart").value;
         yEnd = input.namedItem("yEnd").value;
+        year = input.namedItem("year").value;
+
+        //for the verification that display the modal if there is an error
         verifStart = new Date(`${yStart}-${month}-${day}T12:00:00`);
         verifEnd = new Date(`${yEnd}-${month}-${day}T12:00:00`);
+        verifYear = new Date(`${year}-${month}-${day}T12:00:00`);
+
+
         let currentDate;
-        if (verifStart != "Invalid Date" && verifEnd != "Invalid Date" && yEnd - yStart < 2500) {
+        const invalid = ["Invalid Date"];
+        if (!invalid.includes(verifStart) && !invalid.includes(verifEnd) && !invalid.includes(verifYear) && yEnd - yStart < 1000 && yStart <  yEnd) {
             let fullDay;
             let result = document.getElementById("result");
 
             for (i = yStart; i <= yEnd; i++) {
 
                 currentDate = new Date(`${i}-${month}-${day}T12:00:00`);
-                
+
                 switch (currentDate.getDay()) {
                     case 0:
                         fullDay = "Sunday";
@@ -63,32 +78,35 @@ document.getElementById("dataUser").addEventListener("submit", function (e) {
                         fullDay = "Saturday";
                         break;
                 }
-             
+
                 result.innerHTML += `<div id="${i}" class="result-div" style="position: relative;">In ${i}, your birthday will be on a ${fullDay} </div>` //`In ${i}, your birthday will be on a ${fullDay} <br>`;
 
 
             }
-        } else if (Number(yEnd) - Number(yStart) > 2500) {
-            document.getElementById("pop-up").style.display = "initial"
-            document.getElementById("alert-message").innerHTML = "Too many years to calculate"
+        } else if (Number(yEnd) - Number(yStart) > 1000) {
+            displayModal("Too many years to calculate");
 
-        } else if (verifStart == "Invalid Date" || verifEnd == "Invalid Date") {
-            document.getElementById("pop-up").style.display = "initial"
-            document.getElementById("alert-message").innerHTML = "Please enter numeric value"
+        } else if (verifStart == "Invalid Date" || verifEnd == "Invalid Date" || verifYear == "Invalid Date") {
+            displayModal("Please enter numeric value");
+
+        }  else if (yStart > yEnd){
+            displayModal("Ending year of the calcul must be superior to the Starting year");
         }
 
         //reset options checkboxes to false
-        for (i = 0; i <= 4; i++){
+        for (i = 0; i <= 4; i++) {
             let inpurSelectorArray = document.getElementById("selector").querySelectorAll("input");
             inpurSelectorArray[i].checked = false;
         }
-        
+
     }
     return false;
 })
-// function to close the pop-up
+// function to close the modal
 document.getElementById("close-cross").addEventListener("click", function () {
-    document.getElementById("pop-up").style.display = "none"
+    document.getElementById("pop-up").style.visibility = "hidden"
+    document.getElementById("pop-up").style.opacity = "0"
+    document.getElementById("window").style.top = "35%"
 })
 
 var leap = [];
@@ -104,7 +122,7 @@ document.getElementById("selector").addEventListener("click", function (e) {
             leapCheck = leapCheck.toString();
 
             if (leapCheck.substring(4, 7) === "Feb") document.getElementById(calculatedYear).innerHTML += `<div class="leap" title="${calculatedYear} is a leap year"></div>`;
-            
+
         }
 
     } else if (e.path[0].checked === false && e.path[0].name === "leap") { // stop displaying leap years circle
@@ -115,25 +133,25 @@ document.getElementById("selector").addEventListener("click", function (e) {
 
             if (leapCheck.substring(4, 7) === "Feb") document.getElementById(calculatedYear).innerHTML = document.getElementById(calculatedYear).innerHTML.replace(`<div class="leap" title="${calculatedYear} is a leap year"></div>`, "");
 
-            
+
         }
     }
 
     //week-end modul
-    if(e.path[0].checked === true && e.path[0].name === "week-end"){
-        for (i = 0; i <= yEnd - yStart; i++){
-        let x = document.getElementById(Number(yStart) + i);
-            if (x.innerHTML.substring(36,37) != "S"){
+    if (e.path[0].checked === true && e.path[0].name === "week-end") {
+        for (i = 0; i <= yEnd - yStart; i++) {
+            let x = document.getElementById(Number(yStart) + i);
+            if (x.innerHTML.substring(36, 37) != "S") {
                 x.style.display = "none";
             }
         }
-    } else if (e.path[0].checked === false && e.path[0].name === "week-end"){
-        for (i = 0; i <= yEnd - yStart; i++){
+    } else if (e.path[0].checked === false && e.path[0].name === "week-end") {
+        for (i = 0; i <= yEnd - yStart; i++) {
             let x = document.getElementById(Number(yStart) + i);
-                if (x.innerHTML.substring(36,37) != "S"){
-                    x.style.display = "block";
-                }
+            if (x.innerHTML.substring(36, 37) != "S") {
+                x.style.display = "block";
             }
+        }
     }
 
 })
